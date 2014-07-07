8ad819c2558a78e3aa88cdbc771bf941fa1a7e1a
Jason Miller <jason@developit.ca>
2014-07-06 23:39:00 -0400

    Added full test coverage against a mocked XHR (using sinon.fakeServer)

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,22 +15,81 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+
 		jshint : {
 			options : {
-				browser : true
+				'browser' : true
 			},
-			main : [
-				'<%= pkg.name %>.js'
-			]
+			main : {
+				options : {
+					'-W041' : true,
+					'-W030' : true
+				},
+				src : ['<%= pkg.name %>.js']
+			},
+			test : {
+				options : {
+					'-W030' : true
+				},
+				src : [
+					'test/**/*.js',
+					'!test/js/**/*'
+				]
+			}
+		},
+
+		connect : {
+			test : {
+				options : {
+					port : 9092,
+					base : '.'
+				}
+			}
+		},
+
+		mocha : {
+			test : {
+				options : {
+					run : true,
+					reporter : 'Spec',
+					urls : [
+						'http://localhost:9092/test/index.html'
+					]
+				}
+			}
+		},
+
+		watch : {
+			options : {
+				interrupt : true
+			},
+			src : {
+				files : ['<%= pkg.name %>.js'],
+				tasks : ['default']
+			},
+			test : {
+				files : ['test/**/*'],
+				tasks : ['test']
+			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+	require('load-grunt-tasks')(grunt);
 
 	grunt.registerTask('default', [
+		'build',
+		'test'
+	]);
+
+	grunt.registerTask('build', [
 		'jshint:main',
 		'uglify:main'
 	]);
-	
+
+	grunt.registerTask('test', [
+		'jshint:test',
+		'connect:test',
+		'mocha:test'
+	]);
+
 };
